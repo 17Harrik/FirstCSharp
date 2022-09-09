2eb8f2a26fb12acb6d968d5a960225917fe79e63
Kieran Harrison <17HarriK@fulford.york.sch.uk>
2022-09-09 15:02:41 +0100

    Added comments

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -10,15 +10,17 @@ namespace FirstCSharp
     {
         static void Main(string[] args)
         {
-
+            // Ask the user how many pizzas that want to eat
             Console.WriteLine("How many pizzas would you like to eat? ");
             int answer = int.Parse(Console.ReadLine());
 
+            // Loop for number of times given by the user 
             for (int i = 0; i < answer; i++)
             {
+                // Concatinates the number of pizzas to the string
                 Console.WriteLine("Eat pizza number " + (i+1));
             }
-
+            
 
             Console.ReadLine();
 
